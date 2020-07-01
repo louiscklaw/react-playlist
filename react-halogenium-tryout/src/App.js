4608f979f35e830fc7c5a8d9dc39cd3315b2d6c0
louiscklaw <louiscklaw@gmail.com>
2020-07-01 19:31:42 +0800

    update for add working halogenium,

--- react-halogenium-tryout/src/App.js
+++ react-halogenium-tryout/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { PulseLoader } from 'halogenium';
 
 function App() {
   return (
     <div className="App">
       helloworld
+      yarn add styled-components halogenium
+
+      <PulseLoader color="#26A65B" size="16px" margin="4px"/>
     </div>
   );
 }
