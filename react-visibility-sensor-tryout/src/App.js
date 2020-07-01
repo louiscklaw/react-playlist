09fc8eb8462c2d1f330338929d4eeca33796603c
louiscklaw <louiscklaw@gmail.com>
2020-07-01 19:57:05 +0800

    add working example for rect-visibility-sensor,

--- react-visibility-sensor-tryout/src/App.js
+++ react-visibility-sensor-tryout/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+// const VisibilitySensor = require('react-visibility-sensr');
+import VisibilitySensor from 'react-visibility-sensor'
+
+function onChange (isVisible) {
+  console.log('Element is now %s', isVisible ? 'visible' : 'hidden');
+}
+
+function MyComponent (props) {
+  return (
+    <VisibilitySensor onChange={onChange}>
+      <div>...content goes here...</div>
+    </VisibilitySensor>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       helloworld
+      <pre>
+        yarn add react-visibility-sensor
+        scroll and check the log in browser console
+      </pre>
+
+      <MyComponent />
+
+      <div style={{height: '200vh'}}>
+
+      </div>
     </div>
   );
 }
