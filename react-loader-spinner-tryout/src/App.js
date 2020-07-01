cd8e53f972d1206ab32a5eb5de2a03029f0b3668
louiscklaw <louiscklaw@gmail.com>
2020-07-01 18:08:48 +0800

    update for working spinner,

--- react-loader-spinner-tryout/src/App.js
+++ react-loader-spinner-tryout/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      helloworld spinner
+      yarn add react-loader-spinner
+
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+        />
+
     </div>
   );
 }
