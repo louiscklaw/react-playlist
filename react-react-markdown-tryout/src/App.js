7917c38022f4f361fc7ae77089443254d2104262
louiscklaw <louiscklaw@gmail.com>
2020-07-14 22:19:09 +0800

    init helloworld for react-markdown,

--- react-react-markdown-tryout/src/App.js
+++ react-react-markdown-tryout/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import ReactMarkdown from 'react-markdown'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props){
+  const input = '# This is a header\n\nAnd this is a paragraph'
+
+  return(
+    <>
+      <ReactMarkdown source={input} />
+    </>
+  )
 }
 
-export default App;
+export default App
