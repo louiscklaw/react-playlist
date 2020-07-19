3fbaaff9ffba6922c4a7ec8d2e3fb06d880cd622
louiscklaw <louiscklaw@gmail.com>
2020-07-19 22:51:25 +0800

    update adding react-headroom helloworld,

---
+++ react-headroom/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Headroom from 'react-headroom'
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Headroom>
+        <h1>You can put anything you'd like inside the Headroom Component</h1>
+      </Headroom>
+      <div className="App">
+        helloworld
+      </div>
+    </>
+  );
+}
+
+export default App;
--- scripts/new_from_helloworld.sh
+++ scripts/new_from_helloworld.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
 set -x
-rm -rf * .*
 
-TEST=`echo $PWD|rev |cut -d'/' -f1 |rev`
-git branch -D test/$TEST
-git checkout -b test/$TEST
-
-set -ex
-
-rsync -avz --exclude 'node_modules' ../react-helloworld-tryout/ .
-
-yarn
-
-yarn build
+../scripts/new_from.sh react-helloworld-tryout
