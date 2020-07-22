b5e290c25320688ad21f76d46c9e7d1222547450
louiscklaw <louiscklaw@gmail.com>
2020-07-22 20:20:04 +0800

    update test/react-countdown-tryout,

---
+++ react-countdown-tryout/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import CountdownByMinutes from './components/countdown_by_minutes';
+
+function App() {
+  return (
+    <div className="App">
+      <div>countdown tryout</div>
+
+      <div><CountdownByMinutes /></div>
+
+    </div>
+  );
+}
+
+export default App;
