4dc21066f94776f26b67e5f3385626b7a724ff01
louiscklaw <louiscklaw@gmail.com>
2020-09-10 02:28:10 +0800

    update,

---
+++ react-react-rnd-tryout/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestRnd from './components/TestRnd'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TestRnd />
+    </div>
+  );
+}
+
+export default App;
