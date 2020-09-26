f1d93d638c371c7990407c966abdd5fc154af76e
louiscklaw <louiscklaw@gmail.com>
2020-09-26 19:47:10 +0800

    update and test,

---
+++ react-cal-heatmap-tryout/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import Demo from './Demo'
+
+function App() {
+  return (
+    <div className="App">
+      helloworld
+
+      <Demo />
+
+    </div>
+  );
+}
+
+export default App;
