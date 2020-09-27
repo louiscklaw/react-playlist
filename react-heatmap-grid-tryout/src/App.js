807b2111600186c055930eb48e9dcc065d4df84b
louiscklaw <louiscklaw@gmail.com>
2020-09-27 21:57:19 +0800

    update merger,

--- react-heatmap-grid-tryout/src/App.js
+++ react-heatmap-grid-tryout/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeatMapDemo from './components/heatmap_demo'
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       helloworld
+      <HeatMapDemo />
     </div>
   );
 }
---
+++ react-heatmap-grid-tryout/src/components/heatmap_demo.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import HeatMap from "react-heatmap-grid";
+
+const xLabels = new Array(24).fill(0).map((_, i) => `${i}`);
+
+// Display only even labels
+const xLabelsVisibility = new Array(24)
+  .fill(0)
+  .map((_, i) => (i % 2 === 0 ? true : false));
+
+const yLabels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+const data = new Array(yLabels.length)
+  .fill(0)
+  .map(() =>
+    new Array(xLabels.length).fill(0).map(() => Math.floor(Math.random() * 100))
+  );
+
+function HeatMapDemo(){
+  return(
+    <HeatMap
+      xLabels={xLabels}
+      yLabels={yLabels}
+      xLabelsLocation={"bottom"}
+      xLabelsVisibility={xLabelsVisibility}
+      xLabelWidth={50}
+      data={data}
+      squares
+      onClick={(x, y) => alert(`Clicked ${x}, ${y}`)}
+      cellStyle={(background, value, min, max, data, x, y) => ({
+        background: `rgb(12, 160, 244, ${1 - (max - value) / (max - min)})`,
+        fontSize: "11px",
+        fontFamily: "Arial",
+      })}
+      cellRender={value => value && `${value}%`}
+      title={(value, unit, index) => value && `${value}-${xLabels[index]}`}
+    />
+  )
+}
+
+export default HeatMapDemo
