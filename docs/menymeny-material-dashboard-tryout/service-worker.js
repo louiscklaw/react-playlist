0cf242bb932ac01899cc877e837375c697f52a35
louiscklaw <louiscklaw@gmail.com>
2020-11-23 02:03:19 +0800

    ci: update and test docs subdir,

--- docs/menymeny-material-dashboard-tryout/service-worker.js
+++ docs/menymeny-material-dashboard-tryout/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8edc94da51c09de803c6afdcc63fb4b.js"
+  "/react-playlist/menymeny-material-dashboard-tryout/precache-manifest.ce12e54001aaa348270bd16b393d1ddc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-playlist/menymeny-material-dashboard-tryout/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
