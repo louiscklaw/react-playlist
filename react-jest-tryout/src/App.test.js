cdc82c82b7629dda9057a7b701fa3585572d0379
louiscklaw <louiscklaw@gmail.com>
2020-12-08 18:12:17 +0800

    ci: update for adding jest tsets,

--- react-jest-tryout/src/App.test.js
+++ react-jest-tryout/src/App.test.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-import renderer from 'react-test-renderer';
-import HelloComponent from './components/helloworld'
-
 import helloworld_test from './tests/helloworld_test'
 
 test('renders learn react link', () => {
