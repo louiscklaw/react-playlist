b89c4a09880f7092b203b277145a7dc2808bc3cc
louiscklaw <louiscklaw@gmail.com>
2020-12-20 00:21:17 +0800

    ci: update working helloworkd,

--- react-hot-toast-tryout/src/App.js
+++ react-hot-toast-tryout/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 import './App.css';
 
+
+
+function HelloToast(){
+  return(
+    <div>
+      <Toaster />
+    </div>
+  )
+}
+
 function App() {
+  React.useEffect(()=>{
+    toast('Hello World');
+  },[])
+
   return (
     <div className="App">
-      helloworld
+      <HelloToast />
     </div>
   );
 }
