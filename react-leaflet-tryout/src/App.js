ffb51454b73d660294d7b639e094d0fbc9fc89ff
louiscklaw <louiscklaw@gmail.com>
2020-12-22 23:14:29 +0800

    ci: update leaflet example,

---
+++ react-leaflet-tryout/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
