11e2be68767f94a1a29a24d40f120c23adfbf972
louiscklaw <louiscklaw@gmail.com>
2020-12-23 08:13:49 +0800

    ci: update google maps example,

--- react-google-maps-tryout/src/App.js
+++ react-google-maps-tryout/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { GoogleMap, Marker } from "react-google-maps"
+
 
 import './App.css';
+
+const MyMapComponent = (props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+
+
+
 
 function App() {
   return (
     <div className="App">
-      helloworld
+      <MyMapComponent isMarkerShown />
+      <MyMapComponent isMarkerShown={false} />
     </div>
   );
 }
