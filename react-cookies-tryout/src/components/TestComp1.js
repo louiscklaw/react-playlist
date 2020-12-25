75b338ba2052f704c7f5b0458eefe12831f21499
louiscklaw <louiscklaw@gmail.com>
2020-12-25 16:32:15 +0800

    ci: update for working cookie store,

--- react-cookies-tryout/src/components/TestComp1.js
+++ react-cookies-tryout/src/components/TestComp1.js
@@ -3,19 +3,34 @@ import { useCookies } from 'react-cookie';
 
 import NameForm from './NameForm';
 
+const COOKIE_NAME = 'username'
+
+const encryptText = (text_in) =>{
+  if (text_in && text_in!=''){
+    return text_in.split('').reverse().join('')
+  }
+}
+
+const decryptText = (enc_text_in) => {
+  console.log('enc_text_in', enc_text_in)
+  if (enc_text_in && enc_text_in!=''){
+    return enc_text_in.split('').reverse().join('')
+  }
+}
+
 export default function TestComp1(){
-  const [cookies, setCookie] = useCookies(['name']);
+  const [cookies, setCookie] = useCookies([COOKIE_NAME]);
 
   function onChange(newName) {
-    setCookie('name', newName, { path: '/' });
+    setCookie(COOKIE_NAME, encryptText(newName), { path: '/' });
     // console.log('test');
   }
 
   return(
     <>
       helloworld
-      <NameForm name={cookies.name} onChange={onChange} />
-      {cookies.name && <h1>Hello {cookies.name}!</h1>}
+      <NameForm name={cookies[COOKIE_NAME]} onChange={onChange} />
+      {cookies[COOKIE_NAME] && <h1>Hello {decryptText(cookies[COOKIE_NAME])}!</h1>}
     </>
   )
 }
