a16faa03604f881983d8f77d455f92094de957df
louiscklaw <louiscklaw@gmail.com>
2020-12-25 19:14:53 +0800

    ci: update cache handling,

--- react-cookies-tryout/src/components/TestComp1.js
+++ react-cookies-tryout/src/components/TestComp1.js
@@ -6,6 +6,11 @@ import NameForm from './NameForm';
 const COOKIE_USERNAME = 'username'
 const COOKIE_LOGIN_LOCATION ='location'
 
+const user_cred_cookie_options = {
+  path: '/',
+  maxAge: 3600*24*14
+}
+
 const encryptText = (text_in) =>{
   if (text_in && text_in!=''){
     return text_in.split('').reverse().join('')
@@ -20,12 +25,23 @@ const decryptText = (enc_text_in) => {
 }
 
 export default function TestComp1(){
-  const [cookies, setCookie] = useCookies([COOKIE_USERNAME]);
+  const [cookies, setCookie, removeCookie] = useCookies([COOKIE_USERNAME]);
+
+  React.useEffect(()=>{
+    if (cookies[COOKIE_USERNAME]==null || cookies[COOKIE_USERNAME]=='' ){
+      alert('hello empty cookie_username')
+    }
+  },[])
 
   function onChange(newName) {
-    setCookie(COOKIE_USERNAME, encryptText(newName), { path: '/' });
-    setCookie(COOKIE_LOGIN_LOCATION, encryptText(window.location.hostname), { path: '/' });
+    setCookie(COOKIE_USERNAME, encryptText(newName), user_cred_cookie_options);
+    setCookie(COOKIE_LOGIN_LOCATION, encryptText(window.location.hostname), user_cred_cookie_options);
     // console.log('test');
+  }
+
+  const unsetCookie = () => {
+    removeCookie(COOKIE_USERNAME)
+    removeCookie(COOKIE_LOGIN_LOCATION)
   }
 
   return(
@@ -34,6 +50,8 @@ export default function TestComp1(){
       <NameForm name={decryptText(cookies[COOKIE_USERNAME])} onChange={onChange} />
       {cookies[COOKIE_USERNAME] && <h1>Hello {decryptText(cookies[COOKIE_USERNAME])}!</h1>}
       {cookies[COOKIE_LOGIN_LOCATION] && <h1>Hello {decryptText(cookies[COOKIE_LOGIN_LOCATION])}!</h1>}
+
+      <button onClick={unsetCookie}>unset cookie</button>
     </>
   )
 }
