442ceeb5b1a53b9c4b976c182ce3c83aed7aef22
louiscklaw <louiscklaw@gmail.com>
2021-01-01 19:03:25 +0800

    update for react-jodit,

---
+++ react-jodit-tryout/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import TestExample from './TestJodit'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      helloworld
+      <TestExample />
+    </div>
+  );
+}
+
+export default App;
