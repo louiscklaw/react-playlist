346b0ecf9861f842deaf9c50f7f8b0545066d30d
louiscklaw <louiscklaw@gmail.com>
2021-01-25 01:22:27 +0800

    ci: update, in the middle,

--- meny-manage/src/components/FoodMenuEdit/task.js
+++ meny-manage/src/components/FoodMenuEdit/task.js
@@ -1,39 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 
-import { withStyles } from "@material-ui/styles";
 import { Box, TextField, Button, makeStyles, Input } from "@material-ui/core";
 
 import { Draggable } from "react-beautiful-dnd";
 
-const Container = styled.div`
-  border: 1px solid lightgrey;
-  border-radius: 2px;
-  padding: 8px;
-  margin-bottom: 8px;
-  background-color: ${(props) => (props.isDragging ? "lightGreen" : "white")};
-`;
-
-const Handle = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: orange;
-  border-radius: 4px;
-  margin-right: 8px;
-`;
+const useStyles = makeStyles((theme) => ({
+  helloworld: {},
+  container: (props) => ({
+    border: "1px solid lightgrey",
+    borderRadius: "2px",
+    padding: "8px",
+    marginBottom: "8px",
+    backgroundColor: props.isDragging ? "lightGreen" : "white",
+  }),
+  handle: {
+    width: "20px",
+    height: "20px",
+    backgroundColor: "orange",
+    borderRadius: "4px",
+    marginRight: "8px",
+  },
+}));
 
 function Task({ task, index }) {
+  const classes = useStyles();
   return (
     <>
       <Draggable draggableId={task.id} index={index}>
         {(provided, snapshot) => (
           <Container
+            className={classes.container}
             ref={provided.innerRef}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             isDragging={snapshot.isDragging}
           >
-            <Handle {...provided.dragHandleProps} />
+            <Handle className={classes.handle} {...provided.dragHandleProps} />
             {task.content}
           </Container>
         )}
