f9f47149de7b6f68a0d1d6bc6c6700ec945dcfcb
louiscklaw <louiscklaw@gmail.com>
2021-02-10 15:57:34 +0800

    update for adding react-skeleton,

---
+++ react-skeleton-tryout/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
+import "./App.css";
+
+function App() {
+  let [hello_title, setHelloTitle] = React.useState(null);
+  let [hello_body, setHelloBody] = React.useState(null);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      console.log("hello title");
+      setHelloTitle("hello title");
+
+      setHelloBody("hellobody");
+    }, 3000);
+  }, []);
+
+  return (
+    <SkeletonTheme color="#202020" highlightColor="#444">
+      <div className="App">
+        <div style={{ fontSize: 20, lineHeight: 2 }}>
+          <h1>
+            {hello_title || <Skeleton circle={true} height={50} width={50} />}
+          </h1>
+          {hello_body || <Skeleton count={10} />}
+        </div>
+      </div>
+    </SkeletonTheme>
+  );
+}
+
+export default App;
