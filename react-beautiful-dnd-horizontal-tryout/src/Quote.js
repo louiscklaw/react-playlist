077edf4621690ac0ce5e3454375caf6fc434e6da
louiscklaw <louiscklaw@gmail.com>
2021-02-13 00:48:16 +0800

    update example,

---
+++ react-beautiful-dnd-horizontal-tryout/src/Quote.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import styled from "@emotion/styled";
+
+const grid = 8;
+
+const QuoteItem = styled.div`
+  width: 200px;
+  border: 1px solid grey;
+  margin-bottom: ${grid}px;
+  background-color: lightblue;
+  padding: ${grid}px;
+`;
+
+function Quote({ quote, index }) {
+  return (
+    <Draggable draggableId={quote.id} index={index}>
+      {(provided) => (
+        <QuoteItem
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {quote.content}
+        </QuoteItem>
+      )}
+    </Draggable>
+  );
+}
+
+export default Quote;
