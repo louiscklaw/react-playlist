4d9cd23a00324fdfa238fa18113c03ff4fb50bcc
louiscklaw <louiscklaw@gmail.com>
2021-02-15 01:32:56 +0800

    update,

--- react-beautiful-dnd-functional-component-tryout/src/App.js
+++ react-beautiful-dnd-functional-component-tryout/src/App.js
@@ -5,10 +5,21 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import type { Quote as QuoteType } from "../types";
 
 import QuoteList from "./QuoteList";
+import Quote from "./Quote";
 
 import "./App.css";
 
-const initial = Array.from({ length: 10 }, (v, k) => k).map((k) => {
+const grid = 8;
+
+const QuoteItem = styled.div`
+  width: 50vw;
+  border: 1px solid grey;
+  margin-bottom: ${grid}px;
+  background-color: lightblue;
+  padding: ${grid}px;
+`;
+
+const initial = Array.from({ length: 5 }, (v, k) => k).map((k) => {
   const custom: Quote = {
     id: `id-${k}`,
     content: `Quote ${k}`,
@@ -17,7 +28,6 @@ const initial = Array.from({ length: 10 }, (v, k) => k).map((k) => {
   return custom;
 });
 
-const grid = 8;
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
@@ -49,11 +59,46 @@ function App() {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="list">
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            <QuoteList quotes={state.quotes} />
-            {provided.placeholder}
+      <Droppable droppableId="ml_list" direction="horizontal" type="column">
+        {(lc_provided) => (
+          <div ref={lc_provided.innerRef} {...lc_provided.droppableProps}>
+            {/* master list */}
+            <Droppable droppableId="list">
+              {(lc_provided) => (
+                <div ref={lc_provided.innerRef} {...lc_provided.droppableProps}>
+                  {/* <QuoteList quotes={state.quotes} /> */}
+                  {state.quotes.map((quote: QuoteType, index: number) => {
+                    return (
+                      <>
+                        <Draggable draggableId={quote.id} index={index}>
+                          {(provided) => (
+                            <QuoteItem
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              {quote.content}
+                              <div
+                                style={{
+                                  backgroundImage: `url(https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)`,
+                                  width: "100px",
+                                  height: "100px",
+                                  backgroundSize: "contain",
+                                  backgroundRepeat: "no-repeat",
+                                  backgroundPosition: "center",
+                                }}
+                              />
+                            </QuoteItem>
+                          )}
+                        </Draggable>
+                      </>
+                    );
+                  })}
+                  {lc_provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+            {lc_provided.placeholder}
           </div>
         )}
       </Droppable>
