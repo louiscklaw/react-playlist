f0ccebd4577cc5670f22b7af594a92f4d79af6f0
louiscklaw <louiscklaw@gmail.com>
2021-02-22 08:47:45 +0800

    update in the middle,

---
+++ react-favico-js-tryout/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+import { FavicoContextProvider } from "./contexts/favico";
+import TestButton from "./components/TestButton";
+
+function App() {
+  return (
+    <div className="App">
+      <FavicoContextProvider>
+        <TestButton />
+      </FavicoContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-favico-js-tryout/src/contexts/favico.js
@@ -0,0 +1,41 @@
+import React from "react";
+const Favico = require("favico.js");
+
+export const FavicoContext = React.createContext();
+
+export const FavicoContextProvider = ({ children }) => {
+  let favicon = new Favico({
+    animation: "slide",
+    position: "up",
+    bgColor: "#5CB85C",
+    textColor: "#ff0",
+  });
+  let temp = 0;
+
+  const helloworld = () => {};
+
+  const incCounter = () => {
+    temp = temp + 1;
+    favicon.badge(temp);
+  };
+
+  const descCounter = () => {
+    temp = temp - 1 < 0 ? 0 : temp - 1;
+    favicon.badge(temp);
+  };
+
+  const setZero = () => {
+    favicon.reset();
+  };
+
+  return (
+    <>
+      <FavicoContext.Provider
+        value={{ helloworld, incCounter, descCounter, setZero, favicon }}
+      >
+        <button onClick={setZero}>reset</button>
+        {children}
+      </FavicoContext.Provider>
+    </>
+  );
+};
