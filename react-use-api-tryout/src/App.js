8a963f7fa4c79162e9ab3f5d3851af533216af27
louiscklaw <louiscklaw@gmail.com>
2021-04-09 00:09:41 +0800

    update adding react-use-api,

---
+++ react-use-api-tryout/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import useApi from 'react-use-api'
+
+import './App.css'
+
+function App() {
+  const [data, { loading, error }, request] = useApi({
+    url: 'https://httpbin.org/get?hello=world',
+  })
+
+  return (
+    <>
+      {loading && <div>Loading...</div>}
+      {error && <div>{error.response.data.errMsg}</div>}
+      {data && (
+        <>
+          <div>Hello! </div>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+          <button onClick={request}>Reload</button>
+        </>
+      )}
+    </>
+  )
+}
+
+export default App
