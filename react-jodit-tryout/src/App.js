91ded57aacb880a4e07de047635390dda40e846e
louiscklaw <louis.law@lynked.co.jp>
2021-04-17 19:48:53 +0800

    update and test,

--- react-jodit-tryout/src/App.js
+++ react-jodit-tryout/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 import TestExample from './TestJodit'
 
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       helloworld
       <TestExample />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- react-jodit-tryout/src/TestJodit.js
+++ react-jodit-tryout/src/TestJodit.js
@@ -1,20 +1,20 @@
-import React, {useState, useRef} from 'react';
-import JoditEditor from "jodit-react";
+import React, { useRef, useState } from 'react'
+import JoditEditor from 'jodit-react'
 
 const Example = ({}) => {
-	const editor = useRef(null)
-	const [content, setContent] = useState('')
-	
-	const config = {
-		readonly: false // all options from https://xdsoft.net/jodit/doc/
-	}
-  
-  React.useEffect(()=>{
-    console.log('content',content)
-  },[content])
+  const editor = useRef(null)
+  const [content, setContent] = useState('')
+
+  const config = {
+    readonly: false, // all options from https://xdsoft.net/jodit/doc/
+  }
+
+  React.useEffect(() => {
+    console.log('content', content)
+  }, [content])
 
 
-	return (
+  return (
     <>
       <JoditEditor
         ref={editor}
@@ -22,13 +22,14 @@ const Example = ({}) => {
         config={config}
         tabIndex={1} // tabIndex of textarea
         onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-        onChange={newContent => {}}
+        onChange={newContent => {
+        }}
       />
       <pre>
         {}
       </pre>
     </>
-);
+  )
 }
 
 export default Example
