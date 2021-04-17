fc3c36d8b9d7998eb1adeec801438988d48250d0
louiscklaw <louis.law@lynked.co.jp>
2021-04-17 19:29:27 +0800

    update and test,

--- meme-material-ui-tryout/src/App.js
+++ meme-material-ui-tryout/src/App.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import React from 'react'
 
 
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import { useRoutes } from 'react-router-dom';
-import GlobalStyles from 'src/components/GlobalStyles';
+import 'react-perfect-scrollbar/dist/css/styles.css'
+import { useRoutes } from 'react-router-dom'
+import GlobalStyles from 'src/components/GlobalStyles'
 // import 'src/mixins/chartjs';
 // import theme from 'src/theme';
-
-import routes from 'src/routes';
-
+import routes from 'src/routes'
 
 
 export default function App() {
-  const routing = useRoutes(routes);
+  const routing = useRoutes(routes)
 
   return (
     <>
