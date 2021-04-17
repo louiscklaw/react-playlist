9505db9acd36facddf4ad9cca0444d87288fd067
louiscklaw <louis.law@lynked.co.jp>
2021-04-17 19:50:31 +0800

    update and test,

--- react-snap-tryout/src/App.js
+++ react-snap-tryout/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 import TestExample from './components/TestExample'
 
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       helloworld
       <TestExample />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
