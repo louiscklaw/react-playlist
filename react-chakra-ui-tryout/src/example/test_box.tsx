114921a752c0c6d4893e934bfe3204c5293a692a
louiscklaw <louis.law@lynked.co.jp>
2021-04-24 19:21:56 +0800

    update,

---
+++ react-chakra-ui-tryout/src/example/test_box.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { Box, Image, Badge } from '@chakra-ui/react'
+import { StarIcon } from '@chakra-ui/icons'
+
+function ExampleTestBox() {
+  return <>helloworld</>
+}
+
+export default ExampleTestBox
