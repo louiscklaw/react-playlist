ca2f34003115464375dc38cd76c308a01b5f790c
louiscklaw <louis.law@lynked.co.jp>
2021-04-24 19:17:00 +0800

    update,

--- react-chakra-ui-tryout/init.sh
+++ react-chakra-ui-tryout/init.sh
@@ -3,4 +3,4 @@
 set -ex
 
 yarn
-yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^4
+yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^4 @chakra-ui/icons
