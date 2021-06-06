a456658fc46da1d5f32b0dc9b4dd4bd0cd3d44a0
louiscklaw <louiscklaw@gmail.com>
2021-06-06 18:40:25 +0800

    update and test,

--- scripts/update_main_build_chain.py
+++ scripts/update_main_build_chain.py
@@ -31,7 +31,7 @@ jobs:
 def listYmlFiles(path):
     output = []
     for root, dirs, files in os.walk('.'):
-      tryout_dirs = filter(lambda x: x.find('tryout') > 0, dirs)
+      tryout_dirs = filter(lambda x: re.match('.*tryout$',x) != None , dirs)
       for tryout_dir in tryout_dirs:
         build_yml = '{}/{}/build.yml'.format(PROJ_HOME, tryout_dir)
         if (os.path.exists(build_yml)):
