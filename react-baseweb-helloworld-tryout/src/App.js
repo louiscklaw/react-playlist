65200f5bef916190b2c4f889065744cfcb507752
louiscklaw <louiscklaw@gmail.com>
2021-06-07 14:52:45 +0800

    init baseweb,

---
+++ react-baseweb-helloworld-tryout/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ height: '100%', width: '100%', backgroundColor: 'gold' }}>helloworld</div>
+    </div>
+  )
+}
+
+export default App
