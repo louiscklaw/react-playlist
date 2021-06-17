8f0384d0ff0a75e61cb07df52e1a96faf09a6485
louiscklaw <louiscklaw@gmail.com>
2021-06-17 17:08:28 +0800

    update and test,

---
+++ react-helloworld-tryout/src/components/Checkbox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { GlobalContext } from 'src/contexts/GlobalContext';
+
+export default function HelloComponent() {
+  let { helloworld, helloFunc } = React.useContext(GlobalContext);
+
+  React.useEffect(() => {
+    helloFunc();
+  });
+
+  return (
+    <>
+      <div>HelloComponent</div>
+      <div>{helloworld}</div>
+    </>
+  );
+}
