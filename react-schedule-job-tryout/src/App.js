81346b72c750bf3b0b4a214f3081e450e014a3ec
louiscklaw <louiscklaw@gmail.com>
2021-06-25 11:21:07 +0800

    update,

---
+++ react-schedule-job-tryout/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Schedule from 'react-schedule-job';
+import 'react-schedule-job/dist/index.css';
+
+const styles = {
+  text: {
+    margin: '70px',
+    color: 'skyblue',
+  },
+};
+
+const HelloMsg = () => {
+  return <h1 style={styles.text}>Hello!</h1>;
+};
+
+const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+const App = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const sayHello = () => {
+    setOpen(true);
+  };
+  // this is the function which will run according to your settings
+
+  const jobs = [
+    {
+      fn: sayHello,
+      id: '1',
+      schedule: '* * * * *',
+      // this runs every minutes
+    },
+  ];
+
+  return (
+    <div>
+      <pre>browser timezone {JSON.stringify({ tz })}</pre>
+      <button onClick={(e) => setOpen(false)}>close</button>
+      <Schedule
+        jobs={jobs}
+        timeZone={tz}
+        // "UTC", "local" or "YOUR PREFERRED TIMEZONE",
+        dashboard={{
+          hidden: false,
+          // if true, dashboard is hidden
+        }}
+      />
+      {open && <HelloMsg />}
+    </div>
+  );
+};
+
+export default App;
