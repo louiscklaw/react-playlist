bd4a8cd2330af648a56f7bee2833df8b92df90bf
louiscklaw <louiscklaw@gmail.com>
2021-07-05 12:39:01 +0800

    update for xstate,

--- react-xstate-helloworld-tryout/src/components/Toggle.js
+++ react-xstate-helloworld-tryout/src/components/Toggle.js
@@ -1,13 +1,13 @@
 // import { useMachine } from '../path/to/useMachine';
 import { useMachine } from '@xstate/react';
-import { toggleMachine } from './toggleMachine';
+import { toggleMachine, STATE_INACTIVE, SEND_TOGGLE } from './toggleMachine';
 
 export default function Toggle() {
   const [current, send] = useMachine(toggleMachine);
 
   return (
-    <button onClick={() => send('TOGGLE')}>
-      {current.matches('inactive') ? 'Off' : 'On'}
+    <button onClick={() => send(SEND_TOGGLE)}>
+      {current.matches(STATE_INACTIVE) ? 'Off' : 'On'}
     </button>
   );
 }
--- react-xstate-helloworld-tryout/src/components/toggleMachine.js
+++ react-xstate-helloworld-tryout/src/components/toggleMachine.js
@@ -1,15 +1,21 @@
 import { createMachine } from 'xstate';
 
+const STATE_INACTIVE = 'inactive';
+const STATE_ACTIVE = 'active';
+const SEND_TOGGLE = 'TOGGLE';
+
+export { STATE_ACTIVE, STATE_INACTIVE, SEND_TOGGLE };
+
 // This machine is completely decoupled from React
 export const toggleMachine = createMachine({
   id: 'toggle',
-  initial: 'inactive',
+  initial: STATE_INACTIVE,
   states: {
     inactive: {
-      on: { TOGGLE: 'active' },
+      on: { TOGGLE: STATE_ACTIVE },
     },
     active: {
-      on: { TOGGLE: 'inactive' },
+      on: { TOGGLE: STATE_INACTIVE },
     },
   },
 });
