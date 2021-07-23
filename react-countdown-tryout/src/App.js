2d8aca37febd0aec67dc4f946dfc663ffef2062c
louiscklaw <louiscklaw@gmail.com>
2021-07-23 13:51:43 +0800

    test: 💍 test

--- react-countdown-tryout/src/App.js
+++ react-countdown-tryout/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div>countdown tryout</div>
-      <div>123321</div>
+      <div>1233211</div>
 
       <div>
         <CountdownByMinutes />
