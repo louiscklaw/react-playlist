f590ba3028406e6983b0fac36773b0935f3ed633
louiscklaw <louiscklaw@gmail.com>
2021-07-23 22:57:09 +0800

    update sample,

--- react-baseui-helloworld-tryout/src/__tests__/App.test.js
+++ react-baseui-helloworld-tryout/src/__tests__/App.test.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from 'src/App';
 
+import { ensureMocksReset } from '@shopify/jest-dom-mocks';
+import { installMockStorage } from '@shopify/jest-dom-mocks';
+
+import { requestIdleCallback } from '@shopify/jest-dom-mocks';
+
+beforeEach(() => {
+  requestIdleCallback.mock();
+});
+
+afterEach(() => {
+  requestIdleCallback.restore();
+  ensureMocksReset();
+});
+
 test('helloworld', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/123/i);
