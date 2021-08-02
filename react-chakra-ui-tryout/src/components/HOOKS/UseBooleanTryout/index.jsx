9532c60dff79e77a025ad6535f0960062f04999a
louiscklaw <louiscklaw@gmail.com>
2021-08-02 10:58:04 +0800

    update,

--- react-chakra-ui-tryout/src/components/HOOKS/UseBooleanTryout/index.jsx
+++ react-chakra-ui-tryout/src/components/HOOKS/UseBooleanTryout/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Box, Text, Link, VStack, Code, Grid } from '@chakra-ui/react';
+import { useBoolean } from '@chakra-ui/react';
 
-function TestHelloworld01() {
-  return <>TestHelloworld01</>;
+function UsageOfToggleMethod() {
+  const [flag, setFlag] = useBoolean();
+
+  return (
+    <>
+      <p>Boolean state: {flag.toString()}</p>
+      <button onClick={setFlag.toggle}>
+        Click me to toggle the boolean value
+      </button>
+    </>
+  );
 }
-function TestHelloworld02() {
-  return <>TestHelloworld02</>;
+function UsageOfOnAndOffMethods() {
+  const [flag, setFlag] = useBoolean();
+
+  return (
+    <div onMouseEnter={setFlag.on} onMouseLeave={setFlag.off}>
+      {flag ? 'The flag is ON!' : 'Hover me to turn ON'}
+    </div>
+  );
 }
 function TestHelloworld03() {
   return <>TestHelloworld03</>;
@@ -67,64 +83,10 @@ export function UseBooleanTryout() {
     <div>
       UseBooleanTryout
       <div>
-        TestHelloworld01 <TestHelloworld01 />
+        UsageOfToggleMethod <UsageOfToggleMethod />
       </div>
       <div>
-        TestHelloworld02 <TestHelloworld02 />
-      </div>
-      <div>
-        TestHelloworld03 <TestHelloworld03 />
-      </div>
-      <div>
-        TestHelloworld04 <TestHelloworld04 />
-      </div>
-      <div>
-        TestHelloworld05 <TestHelloworld05 />
-      </div>
-      <div>
-        TestHelloworld06 <TestHelloworld06 />
-      </div>
-      <div>
-        TestHelloworld07 <TestHelloworld07 />
-      </div>
-      <div>
-        TestHelloworld08 <TestHelloworld08 />
-      </div>
-      <div>
-        TestHelloworld09 <TestHelloworld09 />
-      </div>
-      <div>
-        TestHelloworld10 <TestHelloworld10 />
-      </div>
-      <div>
-        TestHelloworld11 <TestHelloworld11 />
-      </div>
-      <div>
-        TestHelloworld12 <TestHelloworld12 />
-      </div>
-      <div>
-        TestHelloworld13 <TestHelloworld13 />
-      </div>
-      <div>
-        TestHelloworld14 <TestHelloworld14 />
-      </div>
-      <div>
-        TestHelloworld15 <TestHelloworld15 />
-      </div>
-      <div>
-        TestHelloworld16 <TestHelloworld16 />
-      </div>
-      <div>
-        TestHelloworld17 <TestHelloworld17 />
-      </div>
-      <div>
-        TestHelloworld18 <TestHelloworld18 />
-      </div>
-      <div>
-        TestHelloworld19 <TestHelloworld19 />
-      </div>
-      <div>
-        TestHelloworld20 <TestHelloworld20 />
+        UsageOfOnAndOffMethods <UsageOfOnAndOffMethods />
       </div>
     </div>
   );
