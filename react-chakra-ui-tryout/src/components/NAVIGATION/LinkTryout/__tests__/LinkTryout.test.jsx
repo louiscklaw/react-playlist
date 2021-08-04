7d226e7fbbbf10ae2f42e4becb791dc78b059c63
louiscklaw <louiscklaw@gmail.com>
2021-08-04 13:26:13 +0800

    update test,

--- react-chakra-ui-tryout/src/components/NAVIGATION/LinkTryout/__tests__/LinkTryout.test.jsx
+++ react-chakra-ui-tryout/src/components/NAVIGATION/LinkTryout/__tests__/LinkTryout.test.jsx
@@ -4,14 +4,19 @@ import renderer from 'react-test-renderer';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { LinkTryout } from '../';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('test LinkTryout tryout', () => {
   test('helloworld', () => {
-    const tree = renderer.create(
-      <ChakraProvider>
-      <LinkTryout />
-      </ChakraProvider>
-      ).toJSON();
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <ChakraProvider>
+            <LinkTryout />
+          </ChakraProvider>
+        </BrowserRouter>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
