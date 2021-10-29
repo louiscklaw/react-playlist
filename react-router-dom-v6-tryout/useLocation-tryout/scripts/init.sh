cb5bb75da5ef23b68fdccc0bb4e2dfe3cb2eb942
louiscklaw <louiscklaw@gmail.com>
2021-10-30 01:31:46 +0800

    update,

--- react-router-dom-v6-tryout/useLocation-tryout/scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-rm -rf node_modules
-
-yarn --dev
-
-yarn add react-router-dom
--- react-router-dom-v6-tryout/useLocation-tryout/src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from 'src/App';
-
-test('helloworld', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/InsideSwitch/i);
-  expect(linkElement).toBeInTheDocument();
-});
