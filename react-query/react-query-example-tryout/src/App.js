5f36c354371f94939e56903a70341f93e7e0b5a6
louiscklaw <louiscklaw@gmail.com>
2021-11-06 12:53:19 +0800

    update,

--- react-query/react-query-example-tryout/src/App.js
+++ react-query/react-query-example-tryout/src/App.js
@@ -1,32 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
+function Example() {
+  let [debug, setDebug] = React.useState(null);
+  const { isLoading, error, data, isFetching } = useQuery('repoData', () =>
+    fetch('http://127.0.0.1:3002/test-get-json').then((res) => res.json())
+  );
+
+  React.useEffect(() => {
+    setDebug({ ...data }, null, 2);
+    console.log('data', data);
+    console.log('data', { ...data });
+  }, [data]);
+
+  if (isLoading) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(debug, null, 2)}</pre>
+      <ReactQueryDevtools initialIsOpen />
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Example />
     </QueryClientProvider>
-  )
-}
-
-function Example() {
-  const { isLoading, error, data, isFetching } = useQuery('repoData', () =>
-    fetch('http://127.0.0.1:3001/test-get-json').then(res => res.json()),
-  )
-
-  if (isLoading) return 'Loading...'
-
-  if (error) return 'An error has occurred: ' + error.message
-
-  return (
-    <div>
-      <h1>{data.name}</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <ReactQueryDevtools initialIsOpen />
-    </div>
-  )
+  );
 }
