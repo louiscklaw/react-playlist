b0b90e45a47cd7c721e06336541bab245ca24838
louiscklaw <louiscklaw@gmail.com>
2021-11-09 17:09:52 +0800

    update,

--- react-detect-offline-tryout/src/components/NetworkStatusDisplay.js
+++ react-detect-offline-tryout/src/components/NetworkStatusDisplay.js
@@ -4,11 +4,20 @@ import { NetworkStatusContext } from 'src/contexts/NetworkStatus';
 
 export default function NetworkStatusDisplay({ url }) {
   const { online } = React.useContext(NetworkStatusContext);
+  let [debug, setDebug] = React.useState(null);
+  React.useEffect(() => {
+    if (online) {
+      setDebug('welcome online');
+    } else {
+      setDebug('welcome offline');
+    }
+  }, [online]);
   return (
     <>
       <div>helloworld url status</div>
       <div>{url}</div>
       <div>{JSON.stringify(online, null, 2)}</div>
+      <div>{JSON.stringify(debug, null, 2)}</div>
     </>
   );
 }
