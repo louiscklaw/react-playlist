93bfcd00e87ce8ce0624b6ba1d56f92108c13ac6
louiscklaw <louiscklaw@gmail.com>
2021-11-16 21:42:57 +0800

    update,

---
+++ storybook-tryout/atlassian-helloworld-tryout/src/components/HelloComponent.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Button from '@atlaskit/button';
+
+export function HelloComponent() {
+  return <Button>Default button</Button>;
+}
---
+++ storybook-tryout/atlassian-helloworld-tryout/src/components/HelloComponent.stories.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { HelloComponent } from './HelloComponent';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/HelloComponent',
+  component: HelloComponent,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <HelloComponent {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
