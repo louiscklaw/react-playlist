4ae5f1214bf3daa64d9f1513835c16a99dc78131
louiscklaw <louiscklaw@gmail.com>
2021-12-11 21:42:47 +0800

    update,

---
+++ react-snowpack-helloworld-tryout/snowpack-react-scss-tryout/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Update the count (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
+  return (
+    <div className={styles.App}>
+      <header className="App-header">
+        <p>
+          Page has been open for <code>{count}</code> seconds.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
