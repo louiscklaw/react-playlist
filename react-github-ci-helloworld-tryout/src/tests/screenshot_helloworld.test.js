2799aeca219195ea88c52b245271d2231fe70986
louiscklaw <louiscklaw@gmail.com>
2021-12-27 19:12:56 +0800

    update github workflow,

--- react-github-ci-helloworld-tryout/src/tests/screenshot_helloworld.test.js
+++ react-github-ci-helloworld-tryout/src/tests/screenshot_helloworld.test.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
 const {desktop_browser} = require('./puppeteer_browser')
+const {INIT_CWD} = process.env
 
 describe(``, () => {
   let manage_browser, manage_page;
@@ -15,10 +16,6 @@ describe(``, () => {
 
   test('', async () => {
     await manage_page.goto('http://localhost:3000');
-
-    await manage_page.screenshot({
-      path: './docs/screenshot.png',
-      fullPage: true,
-    });
+    await manage_page.screenshot({path: `${INIT_CWD}/docs/screenshot.png`,fullPage: true,});
   });
 });
