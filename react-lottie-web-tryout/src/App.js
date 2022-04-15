9a8c4b15872d0dcbd85c3e373459a7bbbaa81e56
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-16 05:44:26 +0800

    update,

--- react-lottie-web-tryout/src/App.js
+++ react-lottie-web-tryout/src/App.js
@@ -3,12 +3,18 @@ import lottie from 'lottie-web';
 // import HelloComponent from 'src/components/HelloComponent';
 import { GlobalContextProvider } from 'src/contexts/GlobalContext';
 import reactLogo from './static/react-logo.json';
+import RedWhiteAndBlueClock from './static/102767-red-white-and-blue-clock.json';
 
 function App({ test_branch = false }) {
   React.useEffect(() => {
     lottie.loadAnimation({
       container: document.querySelector('#react-logo'),
       animationData: reactLogo,
+    });
+
+    lottie.loadAnimation({
+      container: document.querySelector('#red-white-and-blue-clock'),
+      animationData: RedWhiteAndBlueClock,
     });
   }, []);
 
@@ -17,6 +23,10 @@ function App({ test_branch = false }) {
       <div>
         <h1>Hello World</h1>
         <div id="react-logo" style={{ width: 200, height: 200 }} />
+      </div>
+      <div>
+        <h1>Hello World</h1>
+        <div id="red-white-and-blue-clock" style={{ width: 200, height: 200 }} />
       </div>
     </div>
   );
