5775c8feba11c884bd59fe557ced69edf440c404
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-24 21:38:51 +0800

    update react-toastify-tryout,

--- react-toastify-tryout/src/components/toast-test.js
+++ react-toastify-tryout/src/components/toast-test.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function App(){
-  const notify = () => toast("Wow so easy !");
+function App() {
+  const notify = () => toast('Wow so easy !');
 
   return (
     <div>
