24330ded3a6428c8ed9d6ea283927df8f60bd7c3
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-24 21:38:31 +0800

    update react-toastify-tryout,

--- react-toastify-tryout/src/App.js
+++ react-toastify-tryout/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function App(){
-  const notify = () => toast.success('🦄 Wow so easy!', {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-
+function App() {
+  const notify = () =>
+    toast.success('🦄 Wow so easy!', {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
     });
 
   return (
@@ -19,10 +19,8 @@ function App(){
       <button onClick={notify}>Notify !</button>
 
       <ToastContainer />
-
     </div>
   );
 }
-
 
 export default App;
