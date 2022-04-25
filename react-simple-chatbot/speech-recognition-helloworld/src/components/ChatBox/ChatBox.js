30ff72bdb4ab55677fe999214dc0150345e212d8
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-25 11:10:47 +0800

    update react-simple-chatbot,

--- react-simple-chatbot/speech-recognition-helloworld/src/components/ChatBox/ChatBox.js
+++ react-simple-chatbot/speech-recognition-helloworld/src/components/ChatBox/ChatBox.js
@@ -3,28 +3,27 @@ import './ChatBox.css'
 
 function ChatBox() {
   return (
-<ChatBot
-  headerTitle="Speech Recognition"
-  recognitionEnable={true}
-  steps={[
-    {
-      id: '1',
-      message: 'What is your name?',
-      trigger: '2',
-    },
-    {
-      id: '2',
-      user: true,
-      trigger: '3',
-    },
-    {
-      id: '3',
-      message: 'Hi {previousValue}, nice to meet you!',
-      end: true,
-    },
-  ]}
-/>
-
+    <ChatBot
+      headerTitle="Speech Recognition"
+      recognitionEnable={true}
+      steps={[
+        {
+          id: '1',
+          message: 'What is your name?',
+          trigger: '2',
+        },
+        {
+          id: '2',
+          user: true,
+          trigger: '3',
+        },
+        {
+          id: '3',
+          message: 'Hi {previousValue}, nice to meet you!',
+          end: true,
+        },
+      ]}
+    />
   )
 }
 
