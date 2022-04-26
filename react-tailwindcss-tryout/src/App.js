7e62094c68b7c3f97363e03faabdfc07a0326992
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-26 19:21:09 +0800

    update react-tailwindcss-tryout,

---
+++ react-tailwindcss-tryout/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+// import HelloComponent from 'src/components/HelloComponent';
+import { GlobalContextProvider } from 'src/contexts/GlobalContext'
+
+function App({ test_branch = false }) {
+  let helloworld = 'helloworld'
+  let helloanotherworld = '123'
+
+  let helloEmpty
+
+  if (test_branch) {
+    console.log('find true')
+  } else {
+    console.log('find false')
+  }
+
+  console.log('helloEmpty', helloEmpty)
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>09</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
