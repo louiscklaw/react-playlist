00a331ea76737e63b3dc2f24e11e821016fbaf1c
louiscklaw <louiscklaw@gmail.com>
2022-05-04 22:43:23 +0800

    update,

--- gulpfile.js
+++ gulpfile.js
@@ -58,6 +58,48 @@ gulp.task('clear_node_modules', async (done) => {
   });
 });
 
+gulp.task('add_package_scripts', async (done) => {
+  getDirectories('.').then((dirs) => {
+    // let dirs = ['react-form-tryout'];
+    for (let el of dirs) {
+      if (el !== 'node_modules' && el !== '.git' && el !== '.vscode') {
+        let package_json_file = `${el}/package.json`;
+        if (fs.existsSync(package_json_file)) {
+          try {
+            let package_json_content = fs.readFileSync(package_json_file);
+            let package_json = JSON.parse(package_json_content);
+            fs.writeFileSync(
+              package_json_file,
+              JSON.stringify(
+                {
+                  ...package_json,
+                  scripts: {
+                    ...package_json.scripts,
+                    eject: 'react-scripts eject',
+                    prettier_file: 'prettier --write "./src/**/*.{js,jsx}"',
+                    'clear-all': 'rm -rf build node_modules',
+                    're-build':
+                      'rm -rf build node_modules && yarn install && yarn build',
+                    gitUpdate: 'yarn prettier_file && scripts/gitUpdate.sh',
+                    dev: 'yarn --dev && set HTTPS=true&&yarn start',
+                    http: 'serve -s build',
+                  },
+                },
+                null,
+                2
+              )
+            );
+          } catch (error) {
+            console.log(package_json_file);
+            console.error(error);
+          }
+        }
+      }
+    }
+    console.log('done');
+  });
+});
+
 gulp.task(
   'default',
   gulp.series('yarn_all', (done) => {
