40bd8fd7585b4dc0ce01efc566013ed3928a5a73
louiscklaw <louiscklaw@gmail.com>
2022-05-09 06:15:43 -0400

    update react-css-animation-tryout,

--- react-css-animation-tryout/src/contexts/GlobalContext.js
+++ react-css-animation-tryout/src/contexts/GlobalContext.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-export const GlobalContext = React.createContext();
+export const GlobalContext = React.createContext()
 
 export function GlobalContextProvider({ children }) {
-  const helloworld = 'helloworld';
+  const helloworld = 'helloworld'
 
   const helloFunc = () => {
-    console.log('helloFunc');
-  };
+    console.log('helloFunc')
+  }
   return (
     <>
-      <GlobalContext.Provider value={{ helloworld, helloFunc }}>
-        {children}
-      </GlobalContext.Provider>
+      <GlobalContext.Provider value={{ helloworld, helloFunc }}>{children}</GlobalContext.Provider>
     </>
-  );
+  )
 }
