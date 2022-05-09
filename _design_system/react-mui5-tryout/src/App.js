c6976242c67a3b165346fcfed599a12321cde5dc
louiscklaw <louiscklaw@gmail.com>
2022-05-09 07:05:17 -0400

    update,

--- _design_system/react-mui5-tryout/src/App.js
+++ _design_system/react-mui5-tryout/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-// import HelloComponent from 'src/components/HelloComponent';
-import { GlobalContextProvider } from 'src/contexts/GlobalContext';
+import React, { useState } from 'react';
 
 import Button from '@mui/material/Button';
+import { InputAdornment, TextField } from '@mui/material';
+
+import './App.css';
+import { AttachMoney } from '@mui/icons-material';
 
 function App({ test_branch = false }) {
+  let [field_width, setFieldWidth] = useState(200);
   return (
     <div className="App">
       <Button>helloworld</Button>
+      <TextField
+        size="small"
+        sx={{ width: field_width }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AttachMoney />
+            </InputAdornment>
+          ),
+        }}
+        onFocus={() => {
+          setFieldWidth(400);
+        }}
+        onBlur={() => {
+          setFieldWidth(200);
+        }}
+      ></TextField>
     </div>
   );
 }
