7379604cbebbf11e7dea2b58ba2e064f48b721b5
Louis Law <louiscklaw@users.noreply.github.com>
2022-05-24 02:34:33 +0800

    update mui5-helloworld-tryout,

---
+++ _design_system/mui5/helloworld-tryout/src/__tests__/screenshot_helloworld.test.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer')
+
+const headless = false
+const ignoreHTTPSErrors = true
+
+describe(``, () => {
+  let manage_browser, manage_page
+  beforeAll(async () => {
+    manage_browser = await puppeteer.launch({
+      defaultViewport: { width: 1920, height: 1080 },
+      ignoreHTTPSErrors,
+      headless,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-font-subpixel-positioning',
+      ],
+    })
+    manage_page = await manage_browser.newPage()
+  })
+
+  afterAll(async () => {
+    await manage_browser.close()
+  })
+
+  test('', async () => {
+    await manage_page.goto('https://localhost:3000')
+
+    await manage_page.screenshot({
+      path: './docs/screenshot.png',
+      fullPage: true,
+    })
+  })
+})
