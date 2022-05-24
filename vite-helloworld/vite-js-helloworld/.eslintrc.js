365aee0bb7b85ac8a2c89990a9e99cb309164a55
Louis Law <louiscklaw@users.noreply.github.com>
2022-05-24 13:31:36 +0800

    update vite-js-helloworld,

---
+++ vite-helloworld/vite-js-helloworld/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: 'plugin:react/recommended',
+  parserOptions: { ecmaFeatures: { jsx: true }, ecmaVersion: 12, sourceType: 'module' },
+  plugins: ['react'],
+  rules: { 'react/prop-types': 'off', 'react/react-in-jsx-scope': 'off' },
+}
