a9fb95636fd7353b4cac61c9749ac22e5c4b5a27
Louis Law <louiscklaw@users.noreply.github.com>
2022-05-24 15:17:52 +0800

    update vite-js-helloworld,

---
+++ vite-helloworld/vitest-js-helloworld/vitest.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    testTimeout: 50_000,
+    hookTimeout: 50_000,
+    include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+})
