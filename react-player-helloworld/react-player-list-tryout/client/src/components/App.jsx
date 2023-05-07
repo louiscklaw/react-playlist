9b73d8362d34df7cf15c7f67d1186767289ff3ff
Louis Law <louiscklaw@users.noreply.github.com>
2023-05-07 16:32:37 +0800

    update react-player-list-tryout

--- react-player-helloworld/react-player-list-tryout/client/src/components/App.jsx
+++ react-player-helloworld/react-player-list-tryout/client/src/components/App.jsx
@@ -6,17 +6,6 @@ import { typesDef } from './typesDef';
 
 const WS_URL = 'ws://127.0.0.1:8000';
 
-const typesDef_obsolete = {
-  USER_EVENT: 'userevent',
-  CONTENT_CHANGE: 'contentchange',
-  PLAYLIST_CHANGE: 'playlistchange',
-  ADD_YOUTUBE_URL: 'addYoutubeUrl',
-  PLAYER_CONTROL: 'player_control',
-  ADD_URL: 'add_url',
-  STOP_CURRENT_VIDEO: 'stop_current_video',
-  RESUME_CURRENT_VIDEO: 'resume_current_video',
-};
-
 function isUserEvent(message) {
   let evt = JSON.parse(message.data);
   return evt.type === 'userevent';
